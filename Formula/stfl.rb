3aaf95c0273c248ed461921e510c5d2450ce8d56
posativ <info@posativ.org>
2012-04-20 23:07:30 +0200

    add stfl formula

---
+++ Formula/stfl.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Stfl < Formula
+  homepage 'http://www.clifford.at/stfl/'
+  url 'http://www.clifford.at/stfl/stfl-0.22.tar.gz'
+  md5 'df4998f69fed15fabd702a25777f74ab'
+
+  def patches
+    "https://raw.github.com/gist/1556181/stfl.diff"
+  end
+
+  def install
+    system "make install prefix=#{prefix}"
+  end
+end
