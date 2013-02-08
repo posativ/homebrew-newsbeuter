4f59ed2de9637a0775ba2eb6d2a1d615d87ceecc
Martin Zimmermann <info@posativ.org>
2013-02-08 15:42:11 +0100

    Revert "bool -> boolean fix is now upstream"
    This reverts commit d9f836c9ae7c7531ef99c63afcae35fff5b2014f.

--- Formula/newsbeuter.rb
+++ Formula/newsbeuter.rb
@@ -14,7 +14,8 @@ class Newsbeuter < Formula
   depends_on 'gettext'
 
   def patches
-	return [DATA, "https://raw.github.com/gist/2431979/newsbeuter.diff"] if not build.head? 
+	return [DATA] if build.head?
+	return [DATA, "https://raw.github.com/gist/2431979/newsbeuter.diff"]
   end
 
   def install
@@ -23,3 +24,17 @@ class Newsbeuter < Formula
   end
 
 end
+
+__END__
+diff --git a/src/ttrss_api.cpp b/src/ttrss_api.cpp
+index 2cb36d4..0b7ac21 100644
+--- a/src/ttrss_api.cpp
++++ b/src/ttrss_api.cpp
+@@ -228,7 +228,7 @@ rsspp::feed ttrss_api::fetch_feed(const std::string& id) {
+ 		const char * link = json_object_get_string(json_object_object_get(item_obj, "link"));
+ 		const char * content = json_object_get_string(json_object_object_get(item_obj, "content"));
+ 		time_t updated = (time_t)json_object_get_int(json_object_object_get(item_obj, "updated"));
+-		boolean unread = json_object_get_boolean(json_object_object_get(item_obj, "unread"));
++		bool unread = json_object_get_boolean(json_object_object_get(item_obj, "unread"));
+ 
+ 		rsspp::item item;
