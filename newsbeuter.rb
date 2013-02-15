d2e486a993ce370bb6b53937a175e5db42227693
Jon Bernard <jbernard@tuxion.com>
2013-02-15 11:12:52 -0500

    Include OSX-specific patches inline

--- newsbeuter.rb
+++ newsbeuter.rb
@@ -14,10 +14,7 @@ class Newsbeuter < Formula
 
   def patches
     if not build.head? then
-      { :p1 => [
-        "https://raw.github.com/gist/2431979/newsbeuter.diff",
-        DATA
-      ]}
+      { :p1 => DATA }
     end
   end
 
@@ -29,11 +26,44 @@ class Newsbeuter < Formula
 end
 
 __END__
+diff --git a/config.sh b/config.sh
+--- a/config.sh
++++ b/config.sh
+@@ -100,6 +100,8 @@ check_pkg "libcurl" || check_custom "libcurl" "curl-config" || fail "libcurl"
+ check_pkg "libxml-2.0" || check_custom "libxml2" "xml2-config" || fail "libxml2"
+ check_pkg "stfl" || fail "stfl"
+ check_pkg "json" || fail "json"
+-check_custom "ncursesw5" "ncursesw5-config" || fail "ncursesw5"
+ check_ssl_implementation
+ all_aboard_the_fail_boat
++
++echo "CXXFLAGS+=$CPPFLAGS" >> config.mk
++echo "LDFLAGS+=$LDFLAGS" >> config.mk
+diff --git a/src/controller.cpp b/src/controller.cpp
+--- a/src/controller.cpp
++++ b/src/controller.cpp
+@@ -879,8 +879,7 @@ void controller::notify(const std::string& msg) {
+ 		std::cout.flush();
+ 	}
+ 	if (cfg.get_configvalue_as_bool("notify-beep")) {
+-		LOG(LOG_DEBUG, "controller:notify: notifying beep");
+-		::beep();
++		LOG(LOG_DEBUG, "controller:notify: beep unsupported on this platform");
+ 	}
+ 	if (cfg.get_configvalue("notify-program").length() > 0) {
+ 		std::string prog = cfg.get_configvalue("notify-program");
+@@ -931,7 +930,6 @@ void controller::version_information(const char * argv0, unsigned int level) {
+ #if defined(__GNUC__) && defined(__VERSION__)
+ 		std::cout << "Compiler: g++ " << __VERSION__ << std::endl;
+ #endif
+-		std::cout << "ncurses: " << curses_version() << " (compiled with " << NCURSES_VERSION << ")" << std::endl;
+ 		std::cout << "libcurl: " << curl_version()  << " (compiled with " << LIBCURL_VERSION << ")" << std::endl;
+ 		std::cout << "SQLite: " << sqlite3_libversion() << " (compiled with " << SQLITE_VERSION << ")" << std::endl;
+ 		std::cout << "libxml2: compiled with " << LIBXML_DOTTED_VERSION << std::endl << std::endl;
 diff --git a/src/ttrss_api.cpp b/src/ttrss_api.cpp
-index 2cb36d4..0b7ac21 100644
 --- a/src/ttrss_api.cpp
 +++ b/src/ttrss_api.cpp
-@@ -228,7 +228,7 @@ rsspp::feed ttrss_api::fetch_feed(const std::string& id) {
+@@ -220,7 +220,7 @@ rsspp::feed ttrss_api::fetch_feed(const std::string& id) {
  		const char * link = json_object_get_string(json_object_object_get(item_obj, "link"));
  		const char * content = json_object_get_string(json_object_object_get(item_obj, "content"));
  		time_t updated = (time_t)json_object_get_int(json_object_object_get(item_obj, "updated"));
@@ -41,3 +71,15 @@ index 2cb36d4..0b7ac21 100644
 +		bool unread = json_object_get_boolean(json_object_object_get(item_obj, "unread"));
  
  		rsspp::item item;
+ 
+diff --git a/src/view.cpp b/src/view.cpp
+--- a/src/view.cpp
++++ b/src/view.cpp
+@@ -1020,7 +1020,6 @@ void view::handle_cmdline_completion(std::tr1::shared_ptr<formaction> fa) {
+ 	switch (suggestions.size()) {
+ 		case 0:
+ 			LOG(LOG_DEBUG, "view::handle_cmdline_completion: found no suggestion for `%s'", fragment.c_str());
+-			::beep(); // direct call to ncurses - we beep to signal that there is no suggestion available, just like vim
+ 			return;
+ 		case 1:
+ 			suggestion = suggestions[0];
