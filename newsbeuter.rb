1aa746ce77f4b2ee97546118f92d9bc77eb2f846
Jon Bernard <jbernard@tuxion.com>
2013-02-14 22:20:53 -0500

    Cleanup newsbeuter formula

--- newsbeuter.rb
+++ newsbeuter.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Newsbeuter < Formula
   homepage 'http://newsbeuter.org/'
-  head 'https://github.com/akrennmair/newsbeuter.git'
   url 'http://newsbeuter.org/downloads/newsbeuter-2.5.tar.gz'
   sha1 'f3ed4d98c790e881a3782bd7c3e5572b09689bf5'
+  head 'https://github.com/akrennmair/newsbeuter.git', :using => :git
 
   depends_on 'stfl'
   depends_on 'curl'
@@ -14,7 +14,12 @@ class Newsbeuter < Formula
   depends_on 'gettext'
 
   def patches
-    return [DATA, "https://raw.github.com/gist/2431979/newsbeuter.diff"] if not build.head?
+    if not build.head? then
+      { :p1 => [
+        "https://raw.github.com/gist/2431979/newsbeuter.diff",
+        DATA
+      ]}
+    end
   end
 
   def install
